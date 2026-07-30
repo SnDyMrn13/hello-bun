010140758c57aeb145881d76a2561490ede420e5
Cindy <cindino45@gmail.com>
2026-07-29 21:48:06 -0500

    print the date

--- hello.js
+++ hello.js
@@ -3,3 +3,4 @@ let message="Hello from Cindy's computer, " + course + "!";
 console.log(message);
 console.log("this Javascript runs on MY machine.");
 console.log("My name is Cindy Hulitsky.");
+console.log("July 29, 2026");
