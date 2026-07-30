3e46063334f4aae30dc0a1f0e7a72b6c75c722a9
Cindy <cindino45@gmail.com>
2026-07-29 21:08:12 -0500

    start hello-bun with hello script and setup notes

---
+++ hello.js
@@ -0,0 +1,5 @@
+let course="Level 2";
+let message="Hello from Cindy's computer, " + course + "!";
+console.log(message);
+console.log("this Javascript runs on MY machine.");
+console.log("My name is Cindy Hulitsky.");
---
+++ notes.md
@@ -0,0 +1,7 @@
+# My Setup
+-OS: Windows 11 Home, version 25H2
+-Shell: Git Bash
+-Bun version: 1.3.14
+-Editor: Notepad++
+
+
